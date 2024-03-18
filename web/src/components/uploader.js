2ad9e8ba321d900388c3975efe3d0e96bcc1e9ca
Mandal <arpan.a.mandal@accenture.com>
2024-03-18 18:01:14 +0530

    file upload functionality added

--- web/src/components/uploader.js
+++ web/src/components/uploader.js
@@ -1,17 +1,25 @@
 import { useContext, useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { fileToBase64 } from 'file64'
 import { AppContext } from '../context/store'
 
 function Uploader() {
-  const { setUserPhoto } = useContext(AppContext)
+  const { setUserPhoto, setInitImg } = useContext(AppContext)
   const navigate = useNavigate()
 
   const [image, setImage] = useState(null)
   const imageRef = useRef()
 
-  const createImageURL = () => {
+  const createImageURL = async () => {
     if (imageRef.current.files.length > 0) {
       setImage(URL.createObjectURL(imageRef.current.files[0]))
+
+      let temp = await fileToBase64(imageRef.current.files[0])
+      
+      if(temp) {
+        console.log(temp)
+        setInitImg(temp)
+      }
     }
   }
 
@@ -19,6 +27,8 @@ function Uploader() {
     setUserPhoto(image)
     navigate('/tryon')
   }
+
+
 
   return (
     <div>
