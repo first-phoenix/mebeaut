1151a284cd5f1dd9eab649d2791e7b36e1184859
Mandal <arpan.a.mandal@accenture.com>
2024-03-21 12:18:30 +0530

    all deleted

--- backend/makeup_app/tools/helper/faceditector.py
+++
@@ -1,9 +0,0 @@
-from . import faceblendcommon as fbc
-import dlib
-import os
-def detect_landmarks(img):
-    PREDICTOR_PATH= os.getcwd()+r"\tools\helper\shape_predictor_68_face_landmarks.dat"
-    faceDetector=dlib.get_frontal_face_detector()
-    landmarkDetector=dlib.shape_predictor(PREDICTOR_PATH)
-    points=fbc.getLandmarks(faceDetector,landmarkDetector,img)
-    return points
